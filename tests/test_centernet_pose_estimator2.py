2e2f7e84f9c7d7f593ec7b7babc9e99597133234
Florian Hansen <flhansen.93@gmail.com>
2021-07-09 02:20:18 +0200

    Refactor tests.

---
+++ tests/test_centernet_pose_estimator2.py
@@ -0,0 +1,20 @@
+import torch
+import unittest
+
+from modules.models.pose_estimator import CenterNetPoseEstimator2
+
+class TestCenterNetPoseEstimator(unittest.TestCase):
+
+  def test_output_shapes(self):
+    # Create random image
+    x = torch.rand(1, 3, 224, 224)
+
+    net = CenterNetPoseEstimator2(17, backbone='resnet50')
+    output = net(x)
+
+    self.assertEqual(output[0].shape, (1, 17, 56, 56))
+    self.assertEqual(output[1].shape, (1, 34, 56, 56))
+    self.assertEqual(output[2].shape, (1, 2, 56, 56))
+
+if __name__ == '__main__':
+  unittest.main()
